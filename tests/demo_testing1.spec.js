a7b3fb8ad339be93262222a1f17dda0ddd401356
ShraddhaWalke <shraddhawalke0397@gmail.com>
2026-05-31 15:02:00 +0530

    second commit

---
+++ tests/demo_testing1.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { DemoTestingPage } from './pageObjects/demoTestingPage.js';
+
+test('demo testing page object form submit', async ({ page }) => {
+  const demoTestingPage = new DemoTestingPage(page);
+  await demoTestingPage.goto();
+
+  await demoTestingPage.fillContactForm({
+    name: 'Shraddha',
+    email: 'example@gmail.com',
+    address: 'Maharashtra',
+    phone: '1234567789',
+    gender: 'female',
+    weekday: 'monday',
+    country: 'India',
+    color: 'red',
+    animal: 'cat',
+    date: '03/12/2000'
+    
+  });
+
+  await demoTestingPage.submit();
+  await expect(page).toHaveURL('https://testautomationpractice.blogspot.com/');
+});
